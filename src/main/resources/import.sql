368450339f25e992b6db974a02133ffb10caaf2a
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-10-22 15:37:41 -0300

    Create more sql querys for create account, bank, employee

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
-INSERT INTO tb_users (name, last_name, email) VALUES ('William', 'Rios', 'wi@gmail.com')
-INSERT INTO tb_users (name, last_name, email) VALUES ('Zack', 'Hunter', 'zack@gmail.com')
+INSERT INTO tb_users (user_name, last_name, user_email) VALUES ('Jacob', 'Taylor', 'jacob@gmail.com')
+INSERT INTO tb_users (user_name, last_name, user_email) VALUES ('Zack', 'Hunter', 'zack@gmail.com')
 
-INSERT INTO tb_account (balance, account_rank, account_type, account_status, user_id) VALUES (1200, 2, 'Silver', true, 1)
-INSERT INTO tb_account (balance, account_rank, account_type, account_status, user_id) VALUES (1900, 2, 'Gold', true, 2)
+INSERT INTO tb_account (account_balance, account_rank, account_type, account_status, user_id) VALUES (1200, 2, 'Silver', true, 1)
+INSERT INTO tb_account (account_balance, account_rank, account_type, account_status, user_id) VALUES (1900, 2, 'Gold', true, 2)
+
+INSERT INTO tb_bank (bank_name) VALUES ('Bank Broadway')
+
+INSERT INTO tb_employee (employee_name, last_name, employee_email, bank_id) VALUES ('Ambrose', 'Gotobed', 'ambr@gmail.com', 1)
