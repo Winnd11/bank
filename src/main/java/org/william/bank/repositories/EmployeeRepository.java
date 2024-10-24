e4ed3c9908269271f97769f4a5bd098284120c2e
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-10-23 22:24:22 -0300

    Create Employee query methods

--- src/main/java/org/william/bank/repositories/EmployeeRepository.java
+++ src/main/java/org/william/bank/repositories/EmployeeRepository.java
@@ -1,8 +1,23 @@
 package org.william.bank.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.william.bank.entites.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
+	@Query(value = "SELECT * FROM tb_employee", nativeQuery = true)
+	List<Employee> getAllEmployee();
+	
+	@Query(value = "SELECT * FROM tb_employee WHERE employee_name = :name", nativeQuery = true)
+	Employee getEmployeeByName(@Param("name") String name);
+	
+	@Query(value = "SELECT * FROM tb_employee WHERE employee_id = :id", nativeQuery = true)
+	Employee getEmployeeById(@Param("id") Long id);
 
 }
